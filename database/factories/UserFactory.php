8b767bc56637cf48a8547145e4fcd4281056974e
Marcel Stadelmann <m@marceli.to>
2026-04-04 20:33:41 +0200

    Fix UserFactory missing firstname and role fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->firstName(),
+            'name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'admin',
         ];
     }
 
