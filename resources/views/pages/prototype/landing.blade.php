6b8bdfad3ed66c29f476d3ebc3211c61df8d366b
Marcel Stadelmann <m@marceli.to>
2026-04-25 13:58:49 +0200

    wip

--- resources/views/pages/prototype/landing.blade.php
+++ resources/views/pages/prototype/landing.blade.php
@@ -21,7 +21,7 @@
   <header class="bg-red-100/50 border-b border-b-black w-full h-[var(--header-height)] md:h-[var(--header-height-md)] p-16 flex flex-col justify-center shrink-0">
     <div class="relative flex flex-col w-full gap-y-[0.28125rem] leading-none">
       <x-icons.logo.fz class="w-212 h-auto" />
-      <x-icons.logo.a class="w-180 h-auto relative ml-1 transition-transform duration-900 delay-600 ease-out translate-x-[calc(100vw-32px-100%)] will-change-transform [&.is-ready]:translate-x-0 motion-reduce:transition-none motion-reduce:translate-x-0" data-animated-logo />
+      <x-icons.logo.a class="w-180 h-auto relative ml-1 transition-transform duration-900 delay-600 ease-in-out translate-x-[calc(100vw-32px-100%)] will-change-transform [&.is-ready]:translate-x-0 motion-reduce:transition-none motion-reduce:translate-x-0" data-animated-logo />
     </div>
   </header>
 
